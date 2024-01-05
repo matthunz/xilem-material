542b4e620653337a8f4fe632781ea10493e93a77
Matt Hunzinger <matthunz2@gmail.com>
2024-01-04 21:58:35 -0500

    Create basic ripple view

---
+++ src/ripple.rs
@@ -0,0 +1,141 @@
+use std::time::{Duration, Instant};
+use xilem::{
+    vello::{
+        kurbo::{Affine, Circle, Point, Size},
+        peniko::{BlendMode, Brush, Color},
+    },
+    view::{Id, View},
+    widget::{ChangeFlags, Event, Widget},
+    IdPath, Message, MessageResult,
+};
+
+pub struct Ripple {
+    start: Instant,
+    duration: Duration,
+}
+
+impl Ripple {
+    pub fn new() -> Self {
+        Self {
+            start: Instant::now(),
+            duration: Duration::from_millis(300),
+        }
+    }
+}
+
+impl<T, A> View<T, A> for Ripple {
+    type State = Instant;
+    type Element = RippleWidget;
+
+    fn build(&self, cx: &mut xilem::view::Cx) -> (Id, Self::State, Self::Element) {
+        let (id, elem) = cx.with_new_id(|cx| RippleWidget {
+            id_path: cx.id_path().clone(),
+            start: None,
+            duration: self.duration,
+        });
+        (id, Instant::now(), elem)
+    }
+
+    fn rebuild(
+        &self,
+        cx: &mut xilem::view::Cx,
+        prev: &Self,
+        id: &mut Id,
+        state: &mut Self::State,
+        element: &mut Self::Element,
+    ) -> xilem::widget::ChangeFlags {
+        ChangeFlags::PAINT
+    }
+
+    fn message(
+        &self,
+        id_path: &[Id],
+        state: &mut Self::State,
+        message: Box<dyn std::any::Any>,
+        app_state: &mut T,
+    ) -> MessageResult<A> {
+        *state = *message.downcast().unwrap();
+        MessageResult::RequestRebuild
+    }
+}
+
+pub struct RippleWidget {
+    id_path: IdPath,
+    start: Option<(Instant, Point)>,
+    duration: Duration,
+}
+
+impl Widget for RippleWidget {
+    fn event(&mut self, cx: &mut xilem::widget::EventCx, event: &Event) {
+        match event {
+            Event::MouseDown(mouse_event) => {
+                cx.request_paint();
+                self.start = Some((Instant::now(), mouse_event.pos));
+            }
+            _ => {}
+        }
+    }
+
+    fn lifecycle(&mut self, cx: &mut xilem::widget::LifeCycleCx, event: &xilem::widget::LifeCycle) {
+    }
+
+    fn update(&mut self, cx: &mut xilem::widget::UpdateCx) {}
+
+    fn layout(
+        &mut self,
+        cx: &mut xilem::widget::LayoutCx,
+        bc: &xilem::widget::BoxConstraints,
+    ) -> xilem::vello::kurbo::Size {
+        Size::new(200., 200.)
+    }
+
+    fn accessibility(&mut self, cx: &mut xilem::widget::AccessCx) {}
+
+    fn paint(&mut self, cx: &mut xilem::widget::PaintCx, builder: &mut xilem::vello::SceneBuilder) {
+        let max_radius = 100.;
+
+     
+
+        builder.push_layer(
+            BlendMode::new(
+                xilem::vello::peniko::Mix::Clip,
+                xilem::vello::peniko::Compose::Plus,
+            ),
+            1.,
+            Affine::default(),
+            &Circle::new((max_radius / 2., max_radius / 2.), max_radius),
+        );
+
+        builder.fill(
+            xilem::vello::peniko::Fill::EvenOdd,
+            Affine::default(),
+            &Brush::Solid(Color::rgb(0., 0., 1.)),
+            None,
+            &Circle::new((max_radius / 2., max_radius / 2.), max_radius),
+        );
+
+        if let Some((start, pos)) = self.start {
+            let scale = Instant::now()
+                .checked_duration_since(start)
+                .unwrap_or_default()
+                .as_secs_f64()
+                / self.duration.as_secs_f64();
+            let radius = max_radius.min(scale * max_radius);
+            let opacity = if scale < 0.5 {
+                scale
+            } else {
+                0.5f64.min(1. - scale)
+            };
+
+            builder.fill(
+                xilem::vello::peniko::Fill::EvenOdd,
+                Affine::default(),
+                &Brush::Solid(Color::rgba(1., 1., 1., opacity)),
+                None,
+                &Circle::new((pos.x, pos.y), radius),
+            );
+        }
+
+        builder.pop_layer();
+    }
+}
